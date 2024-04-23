8c2ea32c968a6dec203e9003dd32e146316c1734
Lan Vukušič <info@lanvukusic.com>
2024-04-23 22:55:54 +0200

    prediction model for sentene classification

---
+++ lassification_model/model.py
@@ -0,0 +1,24 @@
+# model 384 -> 12
+from torch import nn
+
+
+# Fully connected neural network
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        # self.relu = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+
+        out = self.fc2(out)
+        out = self.relu(out)
+
+        out = self.fc3(out)
+
+        return out
