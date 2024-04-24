12d8ba38706f3656888cfc70df53e764101a81fd
Lan Vukušič <info@lanvukusic.com>
2024-04-24 12:46:21 +0200

    Overfitting problem with my model

--- lassification_model/model.py
+++ lassification_model/model.py
@@ -8,17 +8,37 @@ class NeuralNet(nn.Module):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.65)
+
         self.fc2 = nn.Linear(hidden_size, hidden_size)
-        # self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.65)
+
         self.fc3 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.dropout1(out)
+
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.dropout2(out)
+
+        # out = self.fc22(out)
+        # out = self.relu(out)
+        # out = self.dropout22(out)
+
+        out = self.fc3(out)
+
+        return out
+
+    def embed(self, x):
         out = self.fc1(x)
         out = self.relu(out)
 
         out = self.fc2(out)
         out = self.relu(out)
 
-        out = self.fc3(out)
-
+        # out = self.fc22(out)
+        # out = self.relu(out)
         return out
